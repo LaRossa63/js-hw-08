e3ca0196b7c04c87053589b1330249874d6866ff
LaRossa63 <maksimsevostuanow@mail.ru>
2022-09-05 15:57:26 +0400

    Создание проекта

---
+++ src/data.js
@@ -0,0 +1,46 @@
+export const items = [
+  {
+    img: 'https://vsegda-pomnim.com/uploads/posts/2022-04/1651050842_58-vsegda-pomnim-com-p-kiti-v-okeane-foto-65.png',
+    alt: 'image-1',
+  },
+  {
+    img: 'https://static.tildacdn.com/tild3935-6137-4036-b262-343862353336/Copy_of_4A6A4897.jpg',
+    alt: 'image-2',
+  },
+  {
+    img: 'https://vsegda-pomnim.com/uploads/posts/2022-04/1649124788_6-vsegda-pomnim-com-p-priroda-gor-foto-8.jpg',
+    alt: 'image-3',
+  },
+  {
+    img: 'https://u-stena.ru/upload/iblock/d28/d285da52006f1b3b36cd3bc280fab741.jpg',
+    alt: 'image-4',
+  },
+  {
+    img: 'https://img4.goodfon.ru/wallpaper/original/9/f5/ozero-avstriia-lunersee-vorarlberg-austria-luner-see-gory-ne.jpg',
+    alt: 'image-5',
+  },
+  {
+    img: 'https://vsegda-pomnim.com/uploads/posts/2022-03/1647665820_30-vsegda-pomnim-com-p-gora-ko-foto-31.jpg',
+    alt: 'image-6',
+  },
+  {
+    img: 'https://catherineasquithgallery.com/uploads/posts/2021-03/1614636714_7-p-fon-gori-dlya-fotoshopa-10.jpg',
+    alt: 'image-7',
+  },
+  {
+    img: 'https://i.artfile.ru/2560x1600_508432_[www.ArtFile.ru].jpg',
+    alt: 'image-8',
+  },
+  {
+    img: 'https://s1.1zoom.ru/big3/992/Georgia_Mountains_Forests_Scenery_Ushba_glacier_586507_5120x3413.jpg',
+    alt: 'image-9',
+  },
+  {
+    img: 'https://kartinkin.net/uploads/posts/2022-02/1644872596_8-kartinkin-net-p-kotiki-kartinki-8.jpg',
+    alt: 'image-10',
+  },
+  {
+    img: 'https://regnum.ru/uploads/pictures/news/2019/08/08/regnum_picture_1565235218748739_normal.jpg',
+    alt: 'image-11',
+  },
+];
